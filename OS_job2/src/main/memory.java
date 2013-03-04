25c126615b5edb34fd51b82d0021629b2de67c2e
Donce <admin@l2topai.eu>
2013-03-04 16:09:33 +0200

    Aprašyta atmintis

--- OS_job2/src/main/memory.java
+++ OS_job2/src/main/memory.java
@@ -2,4 +2,28 @@ package main;
 
 public class memory {
 
+	private String userMemory[][];
+	private String superVisorMemory[][];
+	
+	public void inicializeMemory() {
+		this.userMemory = new String[1999][3];
+		this.superVisorMemory = new String[999][3];
+	}
+
+	public String[][] getUserMemory() {
+		return userMemory;
+	}
+
+	public void setUserMemory(String[][] userMemory) {
+		this.userMemory = userMemory;
+	}
+
+	public String[][] getSuperVisorMemory() {
+		return superVisorMemory;
+	}
+
+	public void setSuperVisorMemory(String[][] superVisorMemory) {
+		this.superVisorMemory = superVisorMemory;
+	}
+	
 }
